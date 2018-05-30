ea6cb0cdae0e4bdf39026ba75c6052758156a737
jaracil <pepe@diselpro.com>
2018-05-30 11:31:09 +0200

    Fix Olimex EVB board

--- main/boards.h
+++ main/boards.h
@@ -29,7 +29,6 @@
     #define HW_BOARD "OLIMEX_EVB"
     #define ACTUATORS_GPIO {32, 33}
     #define ACTUATORS_TOUT {10, 10}
-    #define STATUS_LED_GPIO -1
     #define RESET_BUTTON_GPIO 34
     #define I2C_SCL_GPIO 16
     #define I2C_SDA_GPIO 13

b86ee636e9c67e3f4a17265c1b7cbf537d50315d
jaracil <pepe@diselpro.com>
2018-06-08 12:47:19 +0200

    Add "GENERIC A" board and remove "CUSTOM"  and "ESP32-T" boards

--- main/boards.h
+++ main/boards.h
@@ -36,9 +36,9 @@
     #define RTC_DRIVER_PCF8563
 #endif
 
-// ESP32-T with ESP32-BIT
-#ifdef CONFIG_VK_BOARD_ESP32T
-    #define HW_BOARD "ESP32_BIT"
+// ESP32 Generic boards (with free GPIO 0,2,32,33)
+#ifdef CONFIG_VK_BOARD_GENERIC_A
+    #define HW_BOARD "GENERIC"
     #define ACTUATORS_GPIO {32, 33}
     #define ACTUATORS_TOUT {10, 10}
     #define STATUS_LED_GPIO 2
